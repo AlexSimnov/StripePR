8bb05fdad01db7deff9c2016a556811899951310
Симонов Алексей <alxxalxx@mail.ru>
2024-02-27 00:46:43 +0300

    fix bugs

--- backend/item/views.py
+++ backend/item/views.py
@@ -23,7 +23,8 @@ class ProductLandingPageView(TemplateView):
 
     def get_context_data(self, pk, **kwargs):
         product = Item.objects.get(id=pk)
-        context = super(ProductLandingPageView, self).get_context_data(**kwargs)
+        context = super(ProductLandingPageView,
+                        self).get_context_data(**kwargs)
         context.update({
             "product": product,
             "STRIPE_PUBLIC_KEY": settings.STRIPE_P_KEY
