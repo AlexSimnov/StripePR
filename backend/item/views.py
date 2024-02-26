c5f62e24d3ed330c1a01c352979d2ebc7edf9c0b
Симонов Алексей <alxxalxx@mail.ru>
2024-02-26 20:59:28 +0300

    add view of item and payment

---
+++ backend/item/views.py
@@ -0,0 +1,62 @@
+from django.views.generic import TemplateView
+
+from django.views import View
+from django.conf import settings
+import stripe
+from django.http import JsonResponse
+
+from .models import Item
+
+stripe.api_key = settings.STRIPE_S_KEY
+
+
+class SuccessView(TemplateView):
+    template_name = "success.html"
+
+
+class CancelView(TemplateView):
+    template_name = "cancel.html"
+
+
+class ProductLandingPageView(TemplateView):
+    template_name = "landing.html"
+
+    def get_context_data(self, pk, **kwargs):
+        product = Item.objects.get(id=pk)
+        context = super(ProductLandingPageView, self).get_context_data(**kwargs)
+        context.update({
+            "product": product,
+            "STRIPE_PUBLIC_KEY": settings.STRIPE_P_KEY
+        })
+        return context
+
+
+class CreateCheckoutSessionView(View):
+    def post(self, request, *args, **kwargs):
+        product_id = self.kwargs["pk"]
+        product = Item.objects.get(id=product_id)
+        YOUR_DOMAIN = "http://127.0.0.1:8000"
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[
+                {
+                    'price_data': {
+                        'currency': 'usd',
+                        'unit_amount': product.price,
+                        'product_data': {
+                            'name': product.name,
+                        },
+                    },
+                    'quantity': 1,
+                },
+            ],
+            metadata={
+                "product_id": product.id
+            },
+            mode='payment',
+            success_url=YOUR_DOMAIN + '/success/',
+            cancel_url=YOUR_DOMAIN + '/cancel/',
+        )
+        return JsonResponse({
+            'id': checkout_session.id
+        })
